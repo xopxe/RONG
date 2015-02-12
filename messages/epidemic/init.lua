9ea4a202297ef15aa5c52145f7031b3e2aef390e
Jorge <xxopxe@gmail.com>
2015-02-12 18:00:50 -0200

    initial epidemic

---
+++ messages/epidemic/init.lua
@@ -0,0 +1,253 @@
+-- rwalk protocol
+
+local M = {}
+
+local log = require 'lumen.log'
+local sched = require 'lumen.sched'
+local messaging = require 'rong.lib.messaging'
+local encoder_lib = require 'lumen.lib.dkjson' --'lumen.lib.bencode'
+local encode_f, decode_f = encoder_lib.encode, encoder_lib.decode
+local selector = require 'lumen.tasks.selector'
+
+local EVENT_TRIGGER_EXCHANGE = {}
+
+--[[
+-- Process incomming view message
+local view_merge = function(rong, vi)
+  local now = sched.get_time()
+  local view = rong.view
+  local conf = rong.conf
+    
+  -- add all not already registered subscriptions
+  for sid, si in pairs(vi.subs) do
+    local sl = view[sid]
+    if not sl then
+      log('TRW', 'DETAIL', 'Merging subscription: %s', tostring(sid))
+      view:add(sid, si.filter, false)
+      sl = view[sid]
+      sl.meta.last_seen = now
+    end
+  end
+end
+--]]
+
+-- Process incomming token
+local notifs_merge = function (rong, notifs)
+  local now = sched.get_time()
+  local inv = rong.inv
+  local view, view_own = rong.view, rong.view.own
+  
+  for nid, data in pairs(notifs) do
+		local ni=inv[nid]
+		if ni then
+      local meta = ni.meta
+			meta.last_seen = now
+		else	
+      log('TRW', 'DEBUG', 'Merging notification: %s', tostring(nid))
+      inv:add(nid, data, false)
+      rong.messages.init_notification(nid) --FIXME refactor?
+      local n = inv[nid]
+      
+      -- signal arrival of new notification to subscriptions
+      local matches=n.matches
+      for sid, s in pairs(view_own) do
+        if matches[s] then
+          log('RON', 'DEBUG', 'Singalling arrived notification: %s to %s', 
+            tostring(nid), tostring(sid))
+          sched.signal(s, n)          
+        end
+      end
+      
+      
+      if n.target then
+        if n.target == rong.conf.name then
+          log('TRW', 'DEBUG', 'Purging notification on destination: %s', tostring(nid))
+          inv:del(nid)
+        end
+      else
+        --[[
+        --FIXME ???
+        -- if all matching subscriptions are own, can be removed from buffer safely
+        -- (there shouldn't be any, until support for flooding subs is (re)added)
+        local only_own = true
+        for sid, s in pairs(view) do
+          if matches[s] and not view_own[sid] then
+            only_own = false
+            break;
+          end
+        end
+        if only_own then 
+          log('TRW', 'DEBUG', 'Purging notification: %s', tostring(nid))
+          inv:del(nid)
+          --n.meta.delivered = true -- attribute checked when building a token
+        end
+        --]]
+      end
+
+		end
+	end
+end
+
+--in a task to insure atomicity
+sched.sigrun ( {EVENT_TRIGGER_EXCHANGE}, function (_, rong, view)
+  local inv = rong.inv
+  
+  -- Open connection
+  log('TRW', 'DEBUG', 'Sender connecting to: %s:%s', 
+    tostring(view.transfer_ip),tostring(view.transfer_port))
+  local skt = selector.new_tcp_client(view.transfer_ip,view.transfer_port,
+    nil, nil, 'line', 'stream')
+  
+  -- send summary vector
+  local sv = {} -- summary vector
+  for mid, m in pairs (inv) do
+      sv[#sv+1] = mid
+  end
+  local svs = assert(encode_f({sv = sv}))
+  
+  log('TRW', 'DEBUG', 'Sender sumary vector: %i notifs, %i bytes', 
+    #sv, #svs)  
+  local ok, errsend, length = skt:send_sync(svs..'\n')  
+  if not ok then
+    log('TRW', 'DEBUG', 'Sender SV send failed: %s', tostring(errsend))
+    return;
+  end
+  
+  -- read request
+  local reqs, errread = skt.stream:read_line()
+  if not reqs then
+    log('TRW', 'DEBUG', 'Sender REQ read failed: %s', tostring(errread))
+    return;
+  end
+  
+  -- send requested data
+  local out = {}
+  local req = assert(decode_f(reqs))
+  for _, mid in ipairs (req.req) do
+    out[mid] = inv[mid].data
+  end
+  
+  local outs = assert(encode_f({notifs=out}))
+  log('TRW', 'DEBUG', 'Sender DATA built: %i notifs, %i bytes', 
+    #req, #svs)  
+  local okdata, errsenddata, lengthdata = skt:send_sync(outs..'\n')  
+  if not okdata then
+    log('TRW', 'DEBUG', 'Sender DATA send failed: %s', tostring(errsenddata))
+    return;
+  end
+
+  skt:close()
+end)
+
+-- Get handler for reading a token from socket
+local get_receive_token_handler = function (rong)
+  local inv = rong.inv
+  return function(_, skt, err)
+    assert(skt, err)
+    log('TRW', 'DEBUG', 'Receiver accepted: %s', tostring(skt.stream))
+    -- sched.run( function() -- removed, only single client
+      
+    -- read summary vector
+    local ssv, errread = skt.stream:read_line()
+    if not ssv then
+      log('TRW', 'DEBUG', 'Receiver SV read failed: %s', tostring(errread))
+      return;
+    end
+    local sv = assert(decode_f(ssv))
+
+     -- send request
+    local req = {}
+    for _, mid in ipairs(sv.sv) do
+      if not inv[mid] then
+        req[#req+1] = mid
+      end
+    end
+    
+    local sreq = assert(encode_f({req = req}))
+    
+    log('TRW', 'DEBUG', 'Receiver REQ built: %i notifs, %i bytes', 
+      #req, #sreq)  
+    local ok, errsend, length = skt:send_sync(sreq..'\n')  
+    if not ok then
+      log('TRW', 'DEBUG', 'Receiver REQ send failed: %s', tostring(errsend))
+      return;
+    end
+    
+    -- receive data
+    local sdata, errdataread = skt.stream:read_line()
+    if not sdata then
+      log('TRW', 'DEBUG', 'Receiver DATA read failed: %s', tostring(errdataread))
+      return;
+    end
+    local data = assert(decode_f(sdata))
+    
+    notifs_merge(rong, data)
+    
+    -- end)
+  end
+end
+
+local process_incoming_view = function (rong, view)
+  --view_merge( rong, view )
+  sched.signal (EVENT_TRIGGER_EXCHANGE, rong, view)
+end
+
+M.new = function(rong)  
+  local conf = rong.conf
+  local msg = {}
+  
+  rong.token = conf.create_token
+  rong.token_ts = sched.get_time()
+  
+  local tcp_server = selector.new_tcp_server(conf.listen_on_ip, conf.transfer_port, 0, 'stream')
+  conf.listen_on_ip, conf.transfer_port = tcp_server: getsockname()
+  log('TRW', 'INFO', 'Accepting connections on: %s:%s', 
+    tostring(conf.listen_on_ip), tostring(conf.transfer_port)) 
+  sched.sigrun({tcp_server.events.accepted}, get_receive_token_handler(rong))
+
+  msg.broadcast_view = function ()
+    local subs = {}
+    local view_emit = {
+      emitter = conf.name,
+      transfer_ip = conf.listen_on_ip,
+      transfer_port = conf.transfer_port,
+      --notifs = ???,
+    }
+    --[[
+    for sid, s in pairs (rong.view) do
+      local meta = s.meta
+      local sr = {
+        filter = s.filter,
+      }
+      subs[sid] = sr
+    end
+    --]]
+    local ms = assert(encode_f({view=view_emit})) --FIXME tamaño!
+    log('TRW', 'DEBUG', 'Broadcast view: %s', tostring(ms))
+    rong.net:broadcast( ms )
+  end
+  
+  msg.incomming = {
+    view = process_incoming_view,
+  }
+  
+  msg.init_subscription = function (sid)
+    local now = sched.get_time()
+    local s = assert(rong.view[sid])
+    local meta = s.meta
+    meta.init_time = now
+    meta.last_seen = now
+  end
+    
+  msg.init_notification = function (nid)
+    local now = sched.get_time()
+    local n = assert(rong.inv[nid])
+    local meta = n.meta
+    meta.init_time=now
+    meta.last_seen=now
+  end
+
+  return msg
+end
+
+return M
---
+++ test_epidemic.lua
@@ -0,0 +1,77 @@
+--require "strict"
+--look for packages one folder up.
+package.path = package.path .. ";;;../?.lua;../?/init.lua"
+
+local sched = require 'lumen.sched'
+local log = require 'lumen.log'
+log.setlevel('DETAIL', 'RONG')
+log.setlevel('ALL', 'TRW')
+log.setlevel('ALL', 'TEST')
+--log.setlevel('ALL')
+
+local selector = require "lumen.tasks.selector"
+selector.init({service='luasocket'})
+
+
+local n = 1
+local total_nodes = 2
+
+local notifaction_rate = 5    -- secs between notifs
+
+local conf = {
+  name = 'node'..n, --must be unique
+  protocol_port = 8888,
+  listen_on_ip = '10.42.0.'..n, 
+  broadcast_to_ip = '255.255.255.255', --adress used when broadcasting
+  udp_opts = {
+    broadcast	= 1,
+    dontroute	= 0,
+  },
+  send_views_timeout =  5, --5
+  
+  --[[
+  protocol = 'trw',
+  transfer_port = 0,
+  create_token = 'TOKEN@'..n,
+  token_hold_time = 5,
+  --]]
+  
+  protocol = 'epidemic',
+  transfer_port = 0,
+  max_hop_count = 5,
+  buff_sie = 50,
+
+}
+
+math.randomseed(n)
+
+local rong = require 'rong'.new(conf)
+
+local s = rong:subscribe(
+  'SUB1@'..conf.name, 
+  {
+    {'target', '=', 'node'..n },
+  }
+)
+sched.sigrun({s}, function(s, n) 
+  log('TEST', 'INFO', 'ARRIVED FOR %s: %s',tostring(s.id), tostring(n.id))
+  for k, v in pairs (n.data) do
+    log('TEST', 'INFO', '   %s=%s',tostring(k), tostring(v))
+  end
+end)
+
+
+sched.run( function()
+  while true do
+    rong:notificate(
+      'N'..sched.get_time()..'@'..conf.name,
+      {
+        q = 'X',
+        target = 'node'..math.random(total_nodes),
+      }  
+    )
+    sched.sleep(notifaction_rate)
+  end
+end)
+
+sched.loop()
